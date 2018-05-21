d2e23556c8db0a63de094f2ebfdc8efd4184c06b
CrypticHistory <thisisrandom_@hotmail.com>
2018-05-21 19:57:43 -0400

    Delete sqlscript.sql

--- SQL/sqlscript.sql
+++
@@ -1,3 +0,0 @@
-CREATE USER 'alessia'@'localhost' IDENTIFIED BY 'hellokitty17!';
-grant all privileges on dbwgrpg.* to 'alessia'@'%' identified by 'hellokitty17!';
-grant all privileges on dbwgrpg.* to 'alessia'@'localhost' identified by 'hellokitty17!';
